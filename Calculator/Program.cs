32d1163151d5ec30e070adaf9898bb00b7c365a3
hp <hp@BERBATOVCI>
2023-01-18 11:25:04 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Calculator
+{
+    public static void Main()
+    {
+        double num1, num2;
+        string op;
+
+        Console.Write("Enter first number: ");
+        num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter operator: ");
+        op = Console.ReadLine();
+
+        Console.Write("Enter second number: ");
+        num2 = double.Parse(Console.ReadLine());
+
+        switch (op)
+        {
+            case "+":
+                Console.WriteLine("Result: " + (num1 + num2));
+                break;
+            case "-":
+                Console.WriteLine("Result: " + (num1 - num2));
+                break;
+            case "*":
+                Console.WriteLine("Result: " + (num1 * num2));
+                break;
+            case "/":
+                Console.WriteLine("Result: " + (num1 / num2));
+                break;
+            default:
+                Console.WriteLine("Invalid operator");
+                break;
+        }
+    }
+}
